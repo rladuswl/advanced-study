12b5336bde24a91e6c6befcecd0d51deeab4719d
myeondii <khkkgon0118@gmail.com>
2023-01-02 02:54:08 +0900

    프록시 패턴

--- proxy/src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
+++ proxy/src/test/java/hello/proxy/pureproxy/proxy/code/CacheProxy.java
@@ -5,9 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CacheProxy implements Subject {
 
-    private Subject target;
-    private String cacheValue;
+    private Subject target; // 실제 객체 - 프록시가 호출하는 대상
+    private String cacheValue; // 캐시하는 데이터
 
+    // proxy가 RealSubject를 참조하기 위한 코드
     public CacheProxy(Subject target) {
         this.target = target;
     }
@@ -15,7 +16,7 @@ public class CacheProxy implements Subject {
     @Override
     public String operation() {
         log.info("프록시 호출");
-        if (cacheValue == null) {
+        if (cacheValue == null) { // 처음 호출하면 캐시에 데이터가 없으므로..
             cacheValue = target.operation();
         }
         return cacheValue;
