d2ec5a67a960f011e5da64a86c877f01842ba089
kyj <khkkgon0118@gmail.com>
2023-01-06 17:56:28 +0900

    프록시 finish

--- proxy/src/main/java/hello/proxy/config/v6_aop/AopConfig.java
+++ proxy/src/main/java/hello/proxy/config/v6_aop/AopConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
 public class AopConfig {
-
     @Bean
     public LogTraceAspect logTraceAspect(LogTrace logTrace) {
         return new LogTraceAspect(logTrace);
