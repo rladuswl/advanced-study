c75eb3b2e9f9a613fac6fac2f007586471353b7f
kyj <khkkgon0118@gmail.com>
2023-01-06 21:40:34 +0900

    스프링 AOP 구현

---
+++ aop/src/main/java/study/aop/order/aop/Pointcuts.java
@@ -0,0 +1,18 @@
+package study.aop.order.aop;
+
+import org.aspectj.lang.annotation.Pointcut;
+
+public class Pointcuts {
+
+    // study.aop.order 패키지와 하위 패키지
+    @Pointcut("execution(* study.aop.order..*(..))")
+    public void allOrder() {} // pointcut signature
+
+    // 클래스 이름 패턴이 *Service
+    @Pointcut("execution(* *..*Service.*(..))")
+    public void allService() {}
+
+    // allOrder && allService
+    @Pointcut("allOrder() && allService()")
+    public void orderAndService() {}
+}
