8978e65150d21ba203cd2dfcf8a4ed74b148c6bb
kyj <khkkgon0118@gmail.com>
2023-01-15 02:14:23 +0900

    프록시와 내부 호출 - 대안 1,2,3

---
+++ aop/src/test/java/study/aop/internalcall/CallServiceV2Test.java
@@ -0,0 +1,22 @@
+package study.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import study.aop.internalcall.aop.CallLogAspect;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV2Test {
+
+    @Autowired
+    CallServiceV2 callServiceV2;
+
+    @Test
+    void external() {
+        callServiceV2.external();
+    }
+}
